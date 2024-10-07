bcbfc34513cb0c3e24700123431593fbb48dfc51
Alexandr Racov <rakov.eres@gmail.com>
2024-10-07 15:23:19 +0300

    HW08 is almost done

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	args := os.Args
+	if len(args) < 3 {
+		slog.Error("Usage: go run main.go <path-to-dir> <command> [args...]")
+		os.Exit(1)
+	}
+
+	// Чтение переменных окружения из директории
+	env, err := ReadDir(args[1])
+	if err != nil {
+		slog.Error("Error reading directory", slog.String("dir", args[1]), slog.Any("error", err))
+		os.Exit(1)
+	}
+
+	// Запуск команды с переданными аргументами и переменными окружения
+	exitCode := RunCmd(args[2:], env)
+	if exitCode != 0 {
+		slog.Error("Command execution failed", slog.String("command", args[2]), slog.Int("exitCode", exitCode))
+	}
+
+	// Завершаем с кодом возврата команды
+	os.Exit(exitCode)
 }
